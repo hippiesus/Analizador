b7c951b8252fdb2ba89be007ce4583268e1018dd
JC Faundez <juanc.faundez@gmail.com>
2011-07-14 16:36:59 -0400

    Primer commit

--- src/analizador/Analizador.java
+++ src/analizador/Analizador.java
@@ -14,7 +14,7 @@ public class Analizador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(":)");
+        System.out.println("funciona? :)");
         // TODO code application logic here
     }
 }
