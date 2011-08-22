8075e3b87214ae7019d2259a6686a2ad501e1934
JC Faundez <juanc.faundez@gmail.com>
2011-08-22 18:21:25 -0300

    valida que se le pase argumento al ejecutar el main

--- src/analizador/Main.java
+++ src/analizador/Main.java
@@ -14,9 +14,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Analisis analisis= new Analisis();
-        System.out.println("Los archivos se buscaran en:"+ args[0]);
-        analisis.detectarDefectos(args[0]);
-        
+        Analisis analisis = new Analisis();
+        if (args.length != 0) { // valida que se le pase la ruta
+            System.out.println("Los archivos se buscaran en:" + args[0]);
+            analisis.detectarDefectos(args[0]);
+        }
+
     }
 }
