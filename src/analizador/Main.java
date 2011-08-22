97998f6bbaa87ee0e4a69393733fcdefb1d2948b
JC Faundez <juanc.faundez@gmail.com>
2011-08-22 19:55:43 -0300

    agrega / a ruta en el main

--- src/analizador/Main.java
+++ src/analizador/Main.java
@@ -16,8 +16,12 @@ public class Main {
     public static void main(String[] args) {
         Analisis analisis = new Analisis();
         if (args.length != 0) { // valida que se le pase la ruta
+            if (!"/".equals(args[0].substring(args[0].length()-1,args[0].length()))) {
+                args[0]=args[0]+"/";
+            }
             System.out.println("Los archivos se buscaran en:" + args[0]);
             analisis.detectarDefectos(args[0]);
+
         }
 
     }
